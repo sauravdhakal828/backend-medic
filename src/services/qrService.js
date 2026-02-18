67ddeece201df1f9be715c0f93069afa70c41b00
Saurav Dhakal <sauravdhakal828@gmail.com>
2026-02-18 20:41:18 +0545

    URL changed

--- src/services/qrService.js
+++ src/services/qrService.js
@@ -1,7 +1,7 @@
 const QRCode = require("qrcode");
 
 exports.generateQR = async (prescriptionUid) => {
-  const url = `http://localhost:3000/prescription/${prescriptionUid}`;
+  const url = `https://frontend-medic-production.up.railway.app/prescription/${prescriptionUid}`;
   const qr = await QRCode.toDataURL(url);
   return qr;
 };
